c735da71cb42efd9e92b277f9c8e661c95002ec6
Fre Vandeborne <frederic.vandeborne@icapps.com>
2018-08-18 17:13:15 +0200

    Added transitions for Todos

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Todo from './Todo';
 import TodoEditForm from './TodoEditForm';
 import classNames from 'classnames';
+import { Transition } from 'react-spring';
 
 export default class TodoList extends React.Component {
   render() {
@@ -13,42 +14,55 @@ export default class TodoList extends React.Component {
     } else if (this.props.todos.length === 0) {
       return <div className="no-todos">You don't have any Todos yet.</div>;
     } else {
-      const todos = this.props.todos.map(
-        ({ id, title, completed, editing, toDelete }, index) => (
-          <li
-            key={id}
-            className={classNames({
-              todo: true,
-              'delete-confirmation': toDelete,
-              editing: editing,
-            })}
+      return (
+        <ul className="todos">
+          <Transition
+            keys={this.props.todos.map(todo => todo.id)}
+            from={{ opacity: 0, height: 0 }}
+            enter={{ opacity: 1, height: 50 }}
+            leave={{ opacity: 0, height: 0, pointerEvents: 'none' }}
           >
-            {editing === false ? (
-              <Todo
-                id={id}
-                index={index}
-                title={title}
-                toDelete={toDelete}
-                completed={completed}
-                deleteTodo={this.props.deleteTodo}
-                toggleDelete={this.props.toggleDelete}
-                toggleEdit={this.props.toggleEdit}
-                toggleCompleted={this.props.toggleCompleted}
-                moveTodo={this.props.moveTodo}
-              />
-            ) : (
-              <TodoEditForm
-                id={id}
-                title={title}
-                toggleEdit={this.props.toggleEdit}
-                updateTodo={this.props.updateTodo}
-              />
+            {this.props.todos.map(
+              (
+                { id, title, completed, editing, toDelete },
+                index,
+              ) => styles => (
+                <li
+                  key={id}
+                  style={styles}
+                  className={classNames({
+                    todo: true,
+                    'delete-confirmation': toDelete,
+                    editing: editing,
+                  })}
+                >
+                  {editing === false ? (
+                    <Todo
+                      id={id}
+                      index={index}
+                      title={title}
+                      toDelete={toDelete}
+                      completed={completed}
+                      deleteTodo={this.props.deleteTodo}
+                      toggleDelete={this.props.toggleDelete}
+                      toggleEdit={this.props.toggleEdit}
+                      toggleCompleted={this.props.toggleCompleted}
+                      moveTodo={this.props.moveTodo}
+                    />
+                  ) : (
+                    <TodoEditForm
+                      id={id}
+                      title={title}
+                      toggleEdit={this.props.toggleEdit}
+                      updateTodo={this.props.updateTodo}
+                    />
+                  )}
+                </li>
+              ),
             )}
-          </li>
-        ),
+          </Transition>
+        </ul>
       );
-
-      return <ul className="todos">{todos}</ul>;
     }
   }
 }
